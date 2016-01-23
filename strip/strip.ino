6a0e7488131d85e6b6e35a887e5e37e5a60cd91e
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2016-01-23 15:16:58 -0500

    Added basic button functionaility (Tim)

--- strip/strip.ino
+++ strip/strip.ino
@@ -2,32 +2,32 @@
 // released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
 
 #include <Adafruit_NeoPixel.h>
+#include <math.h>
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
-#include <math.h>
 
 // Which pin on the Arduino is connected to the NeoPixels?
 // On a Trinket or Gemma we suggest changing this to 1
 #define PIN            6
-//#define BTNPIN         12
+#define BTNPIN         10
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      60
-#define BEAM_LENGTH    5
+#define BEAM_LENGTH    15
 #define BRIGHT_R       200
 #define MAIN_R         30
 #define OFF            0
 
-
-int off = 0;
+int rainbowInc = 0;
+int rainbowMax = 32;
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int delayval = 100; // delay for half a second
+int delayval = 10; // delay for half a second
 int idx = 0;
 int btnval = 0;
 
@@ -43,66 +43,74 @@ void setup() {
 }
 
 void loop() {
-    /*
-      // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-      int r = 0;
-      int g = 0;
-      int b = 0;
-      for(int i=0;i<NUMPIXELS;i++){
-      r = g = (b++%100);
-      r = (r+2)%100;
+  /*
+    // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+    int r = 0;
+    int g = 0;
+    int b = 0;
+    for(int i=0;i<NUMPIXELS;i++){
+    r = g = (b++%100);
+    r = (r+2)%100;
 
-      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-      pixels.setPixelColor(i, pixels.Color(r,g,b));
-    */
-    int mainR = 30;
-    
-//  btnval = digitalRead(BTNPIN);
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(r,g,b));
+  */
+  int mainR = 30;
+
+  btnval = digitalRead(BTNPIN);
+  Serial.print(btnval);
+  Serial.print("\n");
+  if (btnval == HIGH) {
     displayMain();
     displayBeam();
+  }
+  else{
+    for(int i=0;i<NUMPIXELS;i++){
+     // fullRainbow(i);
+     pixels.setPixelColor(i,pixels.Color(0,0,0));
+    }
     
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  delay(delayval); // Delay for a period of time (in milliseconds).
 }
 
 void displayMain() {
-    for (int i = 0; i < NUMPIXELS; i++) {
-        pixels.setPixelColor(i, pixels.Color(MAIN_R, OFF, OFF));
-    }
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(MAIN_R, OFF, OFF));
+  }
 }
 
 void displayBeam() {
-    for (int beam_step = 0; beam_step < BEAM_LENGTH; ++beam_step) {
-        int brightR = percent(BRIGHT_R, beam_step, BEAM_LENGTH);
-        int pos = (idx+beam_step) % NUMPIXELS;
-        pixels.setPixelColor(pos, pixels.Color(brightR, OFF, OFF));
-    }
-    idx = (idx + 1) % NUMPIXELS;
+  for (int beam_step = 0; beam_step < BEAM_LENGTH; ++beam_step) {
+    int brightR = percent(BRIGHT_R, beam_step, BEAM_LENGTH);
+    int pos = (idx + beam_step) % NUMPIXELS;
+    pixels.setPixelColor(pos, pixels.Color(brightR, OFF, OFF));
+  }
+  idx = (idx + 1) % NUMPIXELS;
 }
 
 bool inRange(int value, int st, int en) {
-    if (en < st) {
-        return (value >= st) || (value <= en);
-    } else {
-        return (value >= st) && (value <= en);
-    }
+  if (en < st) {
+    return (value >= st) || (value <= en);
+  } else {
+    return (value >= st) && (value <= en);
+  }
 }
 
 int percent(int value, int percent, int totalPercent) {
-    int temp = (int) (value * (percent*1.0/totalPercent));
-    temp += 255 - BRIGHT_R;
-    return temp;
+  int temp = (int) (value * (percent * 1.0 / totalPercent));
+  temp += 255 - BRIGHT_R;
+  return temp;
 }
 
-int rainbowInc = 0;
-int rainbowMax = 32;
-
 void fullRainbow(int pixIdx) {
-    int r = Math.sin(0.3 * rainbowInx + 0) * 127 + 128;
-    int g = Math.sin(0.3 * rainbowInx + 2) * 127 + 128;
-    int b = Math.sin(0.3 * rainbowInx + 4) * 127 + 128;
+  int r = sin(0.3 * rainbowInc + 0) * 127 + 128;
+  int g = sin(0.3 * rainbowInc + 2) * 127 + 128;
+  int b = sin(0.3 * rainbowInc + 4) * 127 + 128;
 
-    pixels.setPixel(pixIdx, pixels.Color(r, g, b));
+  pixels.setPixelColor(pixIdx, pixels.Color(r, g, b));
 
-    rainbowInc = (rainbowInx++) % rainbowMax;
+  rainbowInc = (rainbowInc++) % rainbowMax;
 }
