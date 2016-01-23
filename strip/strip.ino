d5c11e05435addbc690b4ec02b6bcd5f4f90e374
Tim Smith <tesmith@gmail.com>
2016-01-23 14:35:39 -0500

    added rainbow function

--- strip/strip.ino
+++ strip/strip.ino
@@ -5,6 +5,7 @@
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
+#include <math.h>
 
 // Which pin on the Arduino is connected to the NeoPixels?
 // On a Trinket or Gemma we suggest changing this to 1
@@ -31,37 +32,50 @@ void setup() {
 #endif
   // End of trinket special code
   Serial.begin(9600);
-//  pinMode(BTNPIN, INPUT);
+  //  pinMode(BTNPIN, INPUT);
   pixels.begin(); // This initializes the NeoPixel library.
 }
 
 void loop() {
-/*
-    // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-    int r = 0;
-    int g = 0;
-    int b = 0;
-    for(int i=0;i<NUMPIXELS;i++){
-    r = g = (b++%100);
-    r = (r+2)%100;
+  /*
+      // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+      int r = 0;
+      int g = 0;
+      int b = 0;
+      for(int i=0;i<NUMPIXELS;i++){
+      r = g = (b++%100);
+      r = (r+2)%100;
 
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(r,g,b));
-*/
-  
-    int mainR = 30;
-    int brightR = 255;
-    int off = 0;
-    
-//  btnval = digitalRead(BTNPIN);
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      pixels.setPixelColor(i, pixels.Color(r,g,b));
+  */
 
-    for (int i = 0; i < NUMPIXELS-NUMBEAM; i++) {
-        pixels.setPixelColor(i, pixels.Color(mainR, off, off));
-    }
-    for (int beam = 0; beam < NUMBEAM; ++beam) {
-        pixels.setPixelColor(idx++, pixels.Color(brightR, off, off));
-        idx = idx % NUMPIXELS;
-    }
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(delayval); // Delay for a period of time (in milliseconds).
+  int mainR = 30;
+  int brightR = 255;
+  int off = 0;
+
+  //  btnval = digitalRead(BTNPIN);
+
+  for (int i = 0; i < NUMPIXELS - NUMBEAM; i++) {
+    pixels.setPixelColor(i, pixels.Color(mainR, off, off));
+  }
+  for (int beam = 0; beam < NUMBEAM; ++beam) {
+    pixels.setPixelColor(idx++, pixels.Color(brightR, off, off));
+    idx = idx % NUMPIXELS;
+  }
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  delay(delayval); // Delay for a period of time (in milliseconds).
 }
+
+int rainbowInc = 0;
+int rainbowMax = 32;
+
+void fullRainbow(int pixIdx) {
+  int r = Math.sin(0.3*rainbowInx + 0) * 127 + 128;
+  int g = Math.sin(0.3*rainbowInx + 2) * 127 + 128;
+  int b = Math.sin(0.3*rainbowInx + 4) * 127 + 128;
+
+  pixels.setPixel(pixIdx, pixels.Color(r,g,b));
+}
+}
+
